996689b3a3b15da5820f7473f526af4a32e0d136
commonsub <commonsub816@gmail.com>
2019-08-06 19:51:45 +0530

    Django REST API

---
+++ snippets/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .serializers import *
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
